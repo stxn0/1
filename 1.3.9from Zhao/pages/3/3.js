2e6a8ab8b922e9d39a0a3a5ddce49d448af4c82a
Zha0-Huan1e <72676308+stxn0@users.noreply.github.com>
2021-02-09 20:06:07 +0800

    Add files via upload

---
+++ 1.3.9from Zhao/pages/3/3.js
@@ -0,0 +1,80 @@
+// pages/3/3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        thingsImage1:"/image/ID.svg",
+        thingsType1:"证件类",
+        thingsImage2:"/image/books.svg ",
+        thingsType2:"书籍类",
+      },
+      {
+        thingsImage1:"/image/dailyUse.svg",
+        thingsType1:"日常类",
+        thingsImage2:"/image/others.svg ",
+        thingsType2:"其他类",
+      },
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ 1.3.9from Zhao/pages/9/9.js
@@ -0,0 +1,80 @@
+// pages/9/9.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        thingsImage1:"/image/notebook.svg",
+        thingsType1:"笔记",
+        thingsImage2:"/image/phone.svg ",
+        thingsType2:"手机",
+        
+      },
+      {
+        thingsImage1:"/image/watch.svg",
+        thingsType1:"手表",
+        thingsImage2:"/image/earphone.svg ",
+        thingsType2:"耳机",
+      },
+    ]
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
